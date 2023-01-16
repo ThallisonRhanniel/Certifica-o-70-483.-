7bb0a0c91036b99c109c781bc0afb67452392431
Thallison Rhanniel <thallisonrlima@gmail.com>
2023-01-16 10:42:39 -0300

    Booleanos

---
+++ ConsoleApp1/5 - Booleanos/Booleanos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1._5___Booleanos
+{
+    internal class Booleanos
+    {
+        public void Executar()
+        {
+            //bool possuiSaldo = 1;
+            bool possuiSaldo = true;
+
+            Console.WriteLine($"possuiSaldo: {possuiSaldo}");
+
+            int dias = DateTime.Now.Day;
+            Console.WriteLine($"dias: {dias}");
+
+            //Atribui a diasPar o valor de uma expressão booleana
+            bool diasPar = (dias % 2 == 0);
+
+            if (diasPar == true) 
+            {
+                Console.WriteLine("dias é um número par");
+            }
+            else
+            {
+                Console.WriteLine("dias é um número ímpar");
+            }
+        }
+    }
+}
