30d7f3f44aff57e2c32c97df0b00c0bceaa8c21e
Thallison Rhanniel <thallisonrlima@gmail.com>
2023-01-15 21:08:30 -0300

    Aula 1 Finalizada

---
+++ ConsoleApp1/Aula1 - criar tipos de valor, incluindo estruturas e enum/1 - Tipos de Valor/TiposDeValor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Aula1___criar_tipos_de_valor__incluindo_estruturas_e_enum._1___Tipos_de_Valor
+{
+    class TiposDeValor : IAulaItem
+    {
+        public void Executar()
+        {
+            int idade = 30;
+
+            System.Int32 copiaIdade = idade;
+
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"copiaIdade: {copiaIdade}");
+
+            idade = 23;
+
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"copiaIdade: {copiaIdade}");
+        }
+    }
+}
