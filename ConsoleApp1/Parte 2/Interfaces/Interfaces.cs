37c466c3977cf0969576c388de1cae16364d65fb
Thallison Rhanniel <thallisonrlima@gmail.com>
2023-01-16 14:51:00 -0300

    Interfaces

---
+++ ConsoleApp1/Parte 2/Interfaces/Interfaces.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Parte_2.Interfaces
+{
+    class Interfaces : IAulaItem
+    {
+        public void Executar()
+        {
+            Ieletrodomestico eletro1 = new Televisao();
+            eletro1.Ligar();
+
+            eletro1 = new Abajur();
+
+        }
+    }
+    interface Ieletrodomestico
+    {
+        void Ligar();
+        void Desligar();
+    }
+
+    interface IIluminacao
+    {
+        double PotenciaDaLampada { get; set; }
+    }
+
+    class Televisao : Ieletrodomestico
+    {
+        public void Desligar()
+        {
+        }
+
+        public void Ligar()
+        {
+        }
+    }
+    class Abajur : Ieletrodomestico, IIluminacao
+    {
+
+        public double PotenciaDaLampada { get; set; }
+
+        public void Desligar()
+        {
+        }
+
+        public void Ligar()
+        {
+        }
+    }
+    class Lanterna : Ieletrodomestico, IIluminacao
+    {
+
+        public double PotenciaDaLampada { get; set; }
+
+        public void Desligar()
+        {
+        }
+
+        public void Ligar()
+        {
+        }
+    }
+}
