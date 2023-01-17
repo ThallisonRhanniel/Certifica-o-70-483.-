f2dfac6cf71a3fdac2910b850287cf20a292cf42
Thallison Rhanniel <thallisonrlima@gmail.com>
2023-01-17 11:59:10 -0300

    métodos

--- ConsoleApp1/Parte 2/7 - Strings/strings.cs
+++ ConsoleApp1/Parte 2/7 - Strings/strings.cs
@@ -8,5 +8,18 @@ namespace ConsoleApp1.Parte_2._7___Strings
 {
     internal class strings
     {
+        public void Executar()
+        {
+            string a = "bom dia";
+            string b = "b";
+            // Adiciona ao conteúdo de "b"
+            b = b + "om dia";
+            Console.WriteLine($"a == b: {a == b}");
+
+            //trecho do código omitido
+
+            string bomDia = "bom dia";
+            Console.WriteLine($"bomDia[4]: {bomDia[4]}");
+        }
     }
 }
