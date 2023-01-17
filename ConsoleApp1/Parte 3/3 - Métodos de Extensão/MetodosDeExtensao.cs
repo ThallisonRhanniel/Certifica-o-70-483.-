109d3ee8a431714826aa33c53b28592968f90be2
Thallison Rhanniel <thallisonrlima@gmail.com>
2023-01-17 12:08:39 -0300

    parametros opcionais

---
+++ ConsoleApp1/Parte 3/3 - Métodos de Extensão/MetodosDeExtensao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Parte_3._3___Métodos_de_Extensão
+{
+    class MetodosDeExtensao : IAulaItem
+    {
+        public void Executar()
+        {
+            Impressora impressora = new Impressora("Este é\r\no meu documento");
+            impressora.ImprimirDocumento();
+        }
+    }
+
+    class Impressora
+    {
+        public string Documento { get; }
+
+        public Impressora(string documento)
+        {
+            this.Documento = documento;
+        }
+
+        public void ImprimirDocumento()
+        {
+            Console.WriteLine();
+            Console.WriteLine(Documento);
+        }
+    }
+}
+}
