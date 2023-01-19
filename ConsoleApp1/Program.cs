8ee6818748c23c2ed96bde9ef7c5ddf1fb72622c
Thallison Rhanniel <thallisonrlima@gmail.com>
2023-01-18 22:01:29 -0300

    Remocão de um código de teste

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-if (Do() | Do())
-    Console.Write(2);
-
-if (Do() || Do())
-    Console.Write(3);
- bool Do()
-{
-    Console.Write(1);
-    return true;
-}
