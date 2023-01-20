630afbfe7bb6c83cf53c210ec61fc7eab174e603
Thallison Rhanniel <thallisonrlima@gmail.com>
2023-01-19 23:49:33 -0300

    classes

---
+++ ConsoleApp1/Parte 2/2 - Classes/classes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Parte_2._2___Classes
+{
+    internal class Exemplo1
+    {
+        private Exemplo2 exemplo = new Exemplo2();
+
+    }
+
+    internal class Exemplo2
+    {
+    }
+}
