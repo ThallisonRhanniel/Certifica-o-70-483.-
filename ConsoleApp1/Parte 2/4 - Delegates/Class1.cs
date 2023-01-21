772b0ad2fd96e0674ab18cc6ed89495adeb9df54
Thallison Rhanniel <thallisonrlima@gmail.com>
2023-01-20 23:52:16 -0300

    Delegate

--- ConsoleApp1/Parte 2/4 - Delegates/Class1.cs
+++ ConsoleApp1/Parte 2/4 - Delegates/Class1.cs
@@ -8,6 +8,26 @@ namespace ConsoleApp1.Parte_2._4___Delegates
 {
     internal class Class1
     {
-        
+        // Declaração
+        public delegate void SimplesDelegate();
+
+        class ExemploDeDelegate
+        {
+            public static void minhaFuncao()
+            {
+                Console.WriteLine("Eu fui chamada por um delegate ...");
+            }
+
+            public static void Main()
+            {
+                // Instanciação
+                SimplesDelegate simplesDelegate = new SimplesDelegate(minhaFuncao);
+
+                // Invocação
+                simplesDelegate();
+                Console.ReadKey();
+            }
+        }
+
     }
 }
