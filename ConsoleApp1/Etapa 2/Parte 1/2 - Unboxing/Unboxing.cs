aa2b02da4fd2818e512a9c1a8abef6d3976bf58f
Thallison Rhanniel <thallisonrlima@gmail.com>
2023-02-14 22:12:37 -0300

    Aula de Boxing e unboxing

---
+++ ConsoleApp1/Etapa 2/Parte 1/2 - Unboxing/Unboxing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Etapa_2._2___Unboxing
+{
+    class Unboxing : IAulaItem
+    {
+        public void Executar()
+        {
+            int numero = 57;      // tipo de valor
+            object caixa = numero;     // boxing
+
+
+            try
+            {
+                int unboxed = (int)caixa;
+                ///int unboxed = (short)caixa;  // tenta fazer unbox
+
+                System.Console.WriteLine("Unboxing OK.");
+            }
+            catch (System.InvalidCastException e)
+            {
+                System.Console.WriteLine("{0} Erro: unboxing incorreto.", e.Message);
+            }
+        }
+    }
+}
