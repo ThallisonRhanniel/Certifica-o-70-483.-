2a060cda3b0ef6d2e402df8d6df9283b8eabde74
Thallison Rhanniel <thallisonrlima@gmail.com>
2023-02-14 22:20:36 -0300

    Trabalhando com dynamic

---
+++ ConsoleApp1/Etapa 2/Parte 3/1 - string, var, object e dynamic/UsandoDynamic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Etapa_2.Parte_3._1___string__var__object_e_dynamic
+{
+    class UsandoDynamic : IAulaItem
+    {
+        public void Executar()
+        {
+            //tipo é definido na compilação
+            string s = "Certificação C#";
+            //tipo é definido na compilação (por inferência)
+            var v = "Certificação C#";
+            //tipo é definido na compilação
+            object o = "Certificação C#";
+
+            Console.WriteLine(s);
+            Console.WriteLine(v);
+            Console.WriteLine(o);
+
+            s = s.ToUpper();
+            v = v.ToUpper();
+            o = ((string)o).ToUpper();
+
+            Console.WriteLine(s);
+            Console.WriteLine(v);
+            Console.WriteLine(o);
+
+            //s = 123;
+            //v = 123;
+            o = 123;
+
+            o = (int)o + 4;
+
+            Console.WriteLine(o);
+
+            //tipo é definido somente na execução
+            dynamic d = "Certificação C#";
+            Console.WriteLine(d);
+            d = d.ToUpper();
+            Console.WriteLine(d);
+            d = 123;
+            Console.WriteLine(d);
+            d = d + 4;
+            Console.WriteLine(d);
+        }
+    }
+}
---
+++ ConsoleApp1/Etapa 2/Parte 3/5 - Interoperabilidade COM/InteropCOM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Etapa_2.Parte_3._5___Interoperabilidade_COM
+{
+    class InteropCOM : IAulaItem
+    {
+        public void Executar()
+        {
+            Type excelType = Type.GetTypeFromProgID("Excel.Application"
+                , true);
+            dynamic excel = Activator.CreateInstance(excelType);
+
+            excel.Visible = true;
+            excel.Workbooks.Add();
+
+            dynamic planilha = excel.ActiveSheet;
+
+            planilha.Cells[1, "A"] = "Alura";
+            planilha.Cells[1, "B"] = "Cursos";
+            planilha.Cells[2, "A"] = "Certificação";
+            planilha.Cells[2, "B"] = "C#";
+            planilha.Columns[1].AutoFit();
+            planilha.Columns[2].AutoFit();
+        }
+    }
+}
